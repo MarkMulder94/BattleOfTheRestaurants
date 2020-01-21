37c20d750c8a8e13cfdb57a01b215fd3962734b1
MarkMulder94 <mark_mulder@hotmail.nl>
2020-01-21 19:29:11 +0100

    Initialize

---
+++ Battle.Library/Manager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Battle.Library
+{
+    public class Manager
+    {
+        public int managerId { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+        public string EmailAddress { get; set; }
+
+        public int restaurantId { get; set; }
+
+
+    }
+}
---
+++ WebClient/src/app/managers/managers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-managers',
+  templateUrl: './managers.component.html',
+  styles: []
+})
+export class ManagersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
